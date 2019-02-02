68007068ef3ff279ea8ebe5e76795fe7a00e0512
PreslavPeev <47269309+PreslavPeev@users.noreply.github.com>
2019-02-02 13:37:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                try
+                {
+                    Console.WriteLine("enter even number");
+                    var n = int.Parse(Console.ReadLine());
+                    if (n % 2 == 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("thenumber is not even");
+                }
+                catch
+                {
+                    Console.WriteLine("invalid number");
+                }
+            }
+            while (true);
+        }
+    }
+}
